9ae31168749a2fcf2aa3734c370d1f6b166bdd56
Jean <jeansflores@outlook.com>
2017-06-14 18:12:43 -0300

    Finalizando a implementação do backend

---
+++ backend/config/routes.js
@@ -0,0 +1,16 @@
+const express = require('express')
+
+module.exports = (server) => {
+
+  //API Routes
+  const router = express.Router()
+  server.use('/api', router)
+
+  // rotas da API
+  const billingCycleService = require('../api/billingCycle/billingCycleService')
+  billingCycleService.register(router, '/billingCycles')
+
+  const billingCycleSummaryService = require('../api/billingCycleSummary/billingCycleSummaryService')
+  router.route('/billingSummary').get(billingCycleSummaryService.getSummary)
+
+}
